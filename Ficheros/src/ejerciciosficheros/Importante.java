53c5c006b62216f518d14e6cfb32a5a83bbde215
Siobhantt <luisa.cardozo@iesnervion.es>
2023-05-20 18:58:40 +0200

    una nota

---
+++ Ficheros/src/ejerciciosficheros/Importante.java
@@ -0,0 +1,16 @@
+package ejerciciosficheros;
+
+public class Importante {
+
+	public static void main(String[] args) {
+		int numentero=0;
+		String numcadena="0";
+		
+		//de cadena a entero
+		numentero = Integer.parseInt(numcadena);
+		
+		//de entero a cadena
+		numcadena = String.valueOf(numentero);
+	}
+
+}
